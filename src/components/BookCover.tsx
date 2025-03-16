20ed9c053f27ac0cf060292d331af0ddca99efca
svash <s.vashkevych@gmail.com>
2025-03-16 16:25:01 +0200

    Layout Book and BookList components

---
+++ src/components/BookCover.tsx
@@ -0,0 +1,38 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import BookCoverSvg from "./BookCoverSvg";
+
+type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide";
+
+const variantStyles: Record<BookCoverVariant, string> = {
+  extraSmall: "cover_extra_small",
+  small: "book-cover_small",
+  medium: "book-cover_medium",
+  regular: "book-cover_regular",
+  wide: "book-cover_wide",
+};
+
+interface Props {
+  className?: string;
+  variant?: BookCoverVariant;
+  color: string;
+  coverImage: string;
+}
+
+const BookCover = ({
+  className,
+  variant = "regular",
+  color = "#012B48",
+  coverImage = "https://placehold.co/400x600.png",
+}: Props) => {
+  return (
+    <div className={cn("relative transition-all duration-300 ease-in-out", variantStyles[variant], className)}>
+      <BookCoverSvg coverColor={color} />
+      <div className="absolute z-10" style={{ left: "12%", width: "87.5%", height: "88%" }}>
+        <Image src={coverImage} alt="Book Cover" fill className="rounded-sm object-fill" />
+      </div>
+    </div>
+  );
+};
+
+export default BookCover;
